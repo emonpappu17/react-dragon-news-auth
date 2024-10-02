ade44b3b26309bf9550e5c889f6d48ef0a42b8f5
emonpappu17 <emonpappu17@hotmail.com>
2024-10-02 20:00:19 +0600

    updated firebase config

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,46 +1,50 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
-import { FaPassport } from "react-icons/fa";
 
-const Login = () => {
-    const { signInUser } = useContext(AuthContext)
-    const location = useLocation();
-    const navigate = useNavigate();
-    console.log('location in the login page', location);
+const Register = () => {
 
+    const { createUser } = useContext(AuthContext)
 
-    const handleLogin = e => {
+    const handleRegister = e => {
         e.preventDefault();
         // console.log(e);
         // console.log(e.currentTarget);
         const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
         const email = form.get('email');
         const password = form.get('password')
-        // console.log(form.get('password'));
+        console.log(email, password, name, photo);
 
-        //Login user
-        signInUser(email, password)
+        //create User
+        createUser(email, password)
             .then(result => {
-                console.log(result.user);
-                e.target.reset();
-
-                // navigate after login
-                navigate(location?.state ? location.state : '/')
-
+                console.log(result.user)
             })
             .catch(error => {
-                console.error(error);
+                console.error(error)
             })
-
     }
 
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className="text-3xl my-10 text-center">Please login</h2>
-            <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+            <h2 className="text-3xl my-10 text-center">Please Register</h2>
+            <form onSubmit={handleRegister} className="md:w-3/4 lg:w-1/2 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input name="name" type="text" placeholder="Name" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Photo URL</span>
+                    </label>
+                    <input name="photo" type="text" placeholder="Photo URL" className="input input-bordered" required />
+                </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
@@ -57,12 +61,12 @@ const Login = () => {
                     </label>
                 </div>
                 <div className="form-control mt-6">
-                    <button className="btn btn-primary">Login</button>
+                    <button className="btn btn-primary">Register</button>
                 </div>
             </form>
-            <p className="text-center mt-5">Do not have an account <Link className="text-blue-600 font-bold" to="/register">Register</Link></p>
+            <p className="text-center mt-5">Already have an account? <Link className="text-blue-600 font-bold" to="/login">Login</Link></p>
         </div>
     );
 };
 
-export default Login;
+export default Register;
